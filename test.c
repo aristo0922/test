47c2c86f24e53bd02cb9b1c4cf0300e82149a70a
Aristotle <dkfud2121@naver.com>
2021-08-02 11:37:41 +0900

    printf commit

--- test.c
+++ test.c
@@ -1,16 +1,8 @@
 import <stdio.h>
 
 int main(){
-	int b=1;
-<<<<<<< HEAD
-	int c = 10;
-=======
-	int a = 5;
-
->>>>>>> refs/remotes/origin/main
-	b++;
-	c =+ b;
 	
+	printf("test");
 
 	return 0;
 }
